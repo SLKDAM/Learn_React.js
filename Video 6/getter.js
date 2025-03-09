ff4ca55a674cb1022c3178cd25003e3f97aae982
SLKDAM <avishkamadhushan841@gmail.com>
2025-03-09 10:30:21 +0530

    getters

---
+++ Video 6/getter.js
@@ -0,0 +1,18 @@
+class Square{
+    constructor(_width){
+        this.width = _width;
+        this.height = _width;
+    }
+
+    getArea(){
+        return this.width * this.height;
+    }
+
+    get area(){
+        return this.width * this.height;
+    }
+}
+
+const newSquare = new Square(15);
+console.log(newSquare.getArea()); // This is a method
+console.log(newSquare.area);  // This is a getter method. It is a property that is a function. It is called like a property, not a method.

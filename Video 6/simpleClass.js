5bef50d272893998c8d229af7ae7da828edd923c
SLKDAM <avishkamadhushan841@gmail.com>
2025-03-09 10:27:15 +0530

    Updated code

--- Video 6/simpleClass.js
+++ Video 6/simpleClass.js
@@ -1,7 +1,26 @@
 class Rectangle{
-    constructor(){
+    constructor(_width, _height,_color){
+        this.width = _width;
+        this.height = _height;
+        this.color = _color;
         console.log("A new rectangle Created.");
     }
+
+    getArea(){
+        return this.width * this.height;
+    }
+
+    aboutMe(){
+        return `I am a rectangle of ${this.width} x ${this.height} and color ${this.color}`;
+    }
+
+
 }
 
-const newRect = new Rectangle();
+const newRect = new Rectangle(10,5,"red");
+console.log(newRect.getArea());
+console.log(newRect.aboutMe());
+
+const newRect2 = new Rectangle(20,10,"blue");
+console.log(newRect2.getArea());
+console.log(newRect2.aboutMe());
